b682e2091e7cf59b5fcdc259680d5ab71bc6d138
scicluna <88948700+scicluna@users.noreply.github.com>
2023-03-13 19:28:47 -0400

    new route structure

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const user = require('./userRoutes')
 const gift = require('./giftRoutes')
+const occasion = require('./occasionRoutes')
+const recipient = require('./recipientRoutes')
 
 router.use('/user', user)
+router.use('/occasion', occasion)
+router.use('/recipient', recipient)
 router.use('/gift', gift)
 
 module.exports = router;
