800415b65fe024a6ed35563cb8959c7f431eb5bb
scicluna <88948700+scicluna@users.noreply.github.com>
2023-03-13 20:10:04 -0400

    Initial gift api routes completed

--- controllers/api/giftRoutes.js
+++ controllers/api/giftRoutes.js
@@ -5,16 +5,16 @@ router.post('/', async(req, res) => {
     try{
     const {name, recipientName} = req.body
 
-    //recipient might be hard to smuggle out - if its just an ID we can raw plug it in, or else we need to process it like below
+    //Probably going to be a massive amount of logic here depending on what rainforest can get us. But the skeleton remains true.
+    //recipient might be hard to smuggle out - if its just an ID we can raw plug it in, or else we need to process it like below.
     const recipientData = await Recipient.findOne({where: {name:recipientName}})
     const plainRecipientData = recipientData.get({plain:true})
 
     const giftData = await Gift.create({name, id:plainRecipientData.id})
 
     //probably redirect it to whereverthey were editting
-    res.redirect('/')
-    }
-    catch(err) {
+    res.status(200).redirect('/')
+    } catch(err) {
         console.log(err)
         res.status(500).json({err})
     }
@@ -22,12 +22,23 @@ router.post('/', async(req, res) => {
 
 router.put('/', async(req, res) => {
     try {
-        
-    } catch (error) {
-        
-    }
+        //if we want it to change fields other than name aswell, we will need to add that into the logic. 
+        //I suspect we will have to grab it from wherever we are going to be doing fetch requests to the api.
+        const {name, id} = req.body
+
+        const giftData = await Gift.update({name}, {where:{id}})
+        if (!giftData) return res.status(400).json("Gift not changed")
+        res.status(200).json("Gift Changed")
+    } catch(err){res.status(500).json(err)}
 })
 
-
+router.delete('/', async(req, res)=>{
+    try{
+        const {id} = req.body
+        const giftData = await Gift.destroy({where: {id}})
+        if (!giftData) return res.status(400).json("Gift not destroyed")
+        res.status(200).json("Destroyed")
+    } catch(err){res.status(500).json(err)}
+})
 
 module.exports = router
